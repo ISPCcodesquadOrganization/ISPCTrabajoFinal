d57977ff4ec1d95b09a6169db77faf4a97f628c9
FrancoR-Orellana <114255752+FrancoR-Orellana@users.noreply.github.com>
2023-06-17 18:10:18 -0300

    Opcion salir
    Se agregan opcion salir y se agrega return a las funciones

--- menu_principal.py
+++ menu_principal.py
@@ -20,7 +20,7 @@ cmd = conexion.cursor()
 def seleccion_usuario():
     print('Por favor ingrese el numero de la opción que desea realizar.')
     menu = ['1 – Mostrar todas las normativas en el registro.', '2 – Buscar una normativa.', '3 – Agregar una normativa.',
-            '4 – Modificar un registro.', '5 – Eliminar un registro.']
+            '4 – Modificar un registro.', '5 – Eliminar un registro.', '6 - Salir del programa']
     
     for i in menu:
         print(i)
@@ -34,41 +34,44 @@ def seleccion_usuario():
 
         print('Se muestran todas las leyes')
         print('------------------------')
-        seleccion_usuario()
+        return seleccion_usuario()
     elif eleccion_usuario == '2':
         #Se inicia funcion de buscador
 
         print('Se da la opcion de escribir al usuario')
         print('------------------------')
-        seleccion_usuario()
+        return seleccion_usuario()
     elif eleccion_usuario == '3':
         #Se dan al usuario los pasos para cargar un nuevo registro
 
         print('Se ha agregado un registro de forma exitosa')
         print('------------------------')
-        seleccion_usuario()
+        return seleccion_usuario()
     elif eleccion_usuario == '4':
         #Se da la opcion al usuario de seleccionar que registro quiere modificar
 
         print('Se ha modificado un registro')
         print('------------------------')
-        seleccion_usuario()
+        return seleccion_usuario()
     elif eleccion_usuario == '5':
         #Se pregunta al usuario que registro quiere eliminar
 
         print('Se ha eliminado un registro')
         print('------------------------')
-        seleccion_usuario()
+        return seleccion_usuario()
+    elif eleccion_usuario == '6':
+        print('Gracias por utilizar el sistema de gestión de normativas')
+        return False
 
     else:
         print('"', eleccion_usuario, '" no es una opcion valida')
         print('------------------------')
-        seleccion_usuario()
+        return seleccion_usuario()
 
 
 
 #menu de inicio
 
-print('Bienvenido al sistema de consulta y edición de normativas.')
+print('Bienvenido al sistema de gestión de normativas.')
 seleccion_usuario()
 
