17bf324da73c478f30cddc14b6371e1a9f87b04f
BelenGiampietro <belgiampietro@gmail.com>
2023-06-20 19:17:26 -0300

    modificacion codigo

--- appPython/update.py
+++ appPython/update.py
@@ -15,14 +15,19 @@ class Ley:
         else:
             return 'Órgano legislativo no definido'
 
-    def actualizar_datos(self, nuevos_datos):
-        self.datos.update(nuevos_datos)
+    def actualizar_datos(self):
+        nuevo_titulo = input("Ingrese el nuevo título de la ley: ")
+        nuevo_tipo = input("Ingrese el nuevo tipo de la ley (nacional/provincial): ")
+
+        self.titulo = nuevo_titulo
+        self.tipo = nuevo_tipo
+        self.organo_legislativo = self.obtener_organo_legislativo()
 
 
-# Ejemplo de uso
+"""# Ejemplo de uso
 ley = Ley('Ley de Educación', 'nacional')
 print(ley.datos)  # Salida: {'titulo': 'Ley de Educación', 'tipo': 'nacional', 'organo_legislativo': 'Congreso de la Nación'}
 
 nuevos_datos = {'titulo': 'Nueva Ley de Educación', 'tipo': 'provincial'}
 ley.actualizar_datos(nuevos_datos)
-print(ley.datos)  # Salida: {'titulo': 'Nueva Ley de Educación', 'tipo': 'provincial', 'organo_legislativo': 'Legislatura de Córdoba'}
+print(ley.datos)  # Salida: {'titulo': 'Nueva Ley de Educación', 'tipo': 'provincial', 'organo_legislativo': 'Legislatura de Córdoba'}"""
