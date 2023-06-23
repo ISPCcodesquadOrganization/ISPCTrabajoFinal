5a1021833c8201f7dd096432835a7729ac4bb6f5
renzocba <renzo.medina39@gmail.com>
2023-06-23 12:50:03 -0300

    agregado Insert

--- appPython/AppInsert.py
+++ appPython/AppInsert.py
@@ -1,24 +1,17 @@
-import sqlite3
+def agregarRegistro():
 
-def agregar_ley():
-    # Establecer conexión con la base de datos
-    conexion = sqlite3.connect('')
-    cursor = conexion.cursor()
+    cursor = db.cursor()
     
-    # Obtener los detalles de la nueva ley desde el usuario
-    nombre = input("Ingrese el nombre de la ley: ")
-    descripcion = input("Ingrese la descripción de la ley: ")
-    
-    # Ejecutar la sentencia INSERT para agregar la nueva ley
-    sentencia = "INSERT INTO nombre_de_tabla (nombre, descripcion) VALUES (?, ?)"
-    valores = (nombre, descripcion)
-    cursor.execute(sentencia, valores)
-    
-    # Confirmar y cerrar la conexión
-    conexion.commit()
-    conexion.close()
-    
-    print("La ley se ha agregado correctamente.")
+    #Ingreso de datos para nuevo registro
 
-# Ejemplo de uso
-agregar_ley()
+    numero_normativa = input('Ingrese el numero correspondiente a la normativa que desea cargar: ')
+    fecha = input('Ingrese la fecha de creacion de la normativa (el formato debe ser AÑO-MES-DÍA): ')
+    descripcion = input('Ingrese una descripcion de la normativa (hasta mil caracteres): ')
+    idNormativa = input('Ingrese el ID correspondiente al tipo de normativa (101 - Ley, 102 - Resolución, 103 - Decreto): ')
+    idCategoria = input('Ingrese el ID de la categoria (201 - Laboral, 202 - Penal, 203 - Civil, 204 - Comercial, 205 - Derecho Informático): ')
+    idJurisdiccion = input('Ingrese el ID de la Jurisdicción (301 - Nacional, 302 - Provincial, 303 - Municipal): ')
+
+
+    sentencia = "INSERT INTO normativas (numero_normativa, fecha, descripcion, Normativas_idNormativa, categorias_idcategoria, jurisdiccion_idjurisdiccion) VALUES ('{}', '{}', '{}', '{}', '{}', '{}')".format(numero_normativa, fecha, descripcion, idNormativa, idCategoria, idJurisdiccion)
+    cursor.execute(sentencia)
+    db.commit()
